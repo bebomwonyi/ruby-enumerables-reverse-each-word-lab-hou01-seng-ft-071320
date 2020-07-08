288ff0f60887115ac445ceca1bb2e18105e7a177
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-08 18:22:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,13 @@
 def reverse_each_word(sentence)
-  sentence.reverse
+  ss=sentence.reverse
+  sss=ss.split
+  ssss=[]
+  sss.each do |a|
+    ssss.unshift(a.split(''))
+  end
+  b=[]
+  ssss.each do |a|
+    b << a.join('')
+  end
+  return bb=b.join(' ')
 end
