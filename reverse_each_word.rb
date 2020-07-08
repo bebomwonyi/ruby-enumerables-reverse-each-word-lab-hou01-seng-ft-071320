4fc1704d6c9a35a4273daf8e5ba04cc2bba41840
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-08 20:25:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   ss=s.split
   ssss=[]
   sss=ss.collect {|a|a.reverse}
-  sss.each {|a| ssss.unshift(a)}
+  sss.each {|a| ssss << (a)}
   a = ssss.join(' ')
   return a
 end
